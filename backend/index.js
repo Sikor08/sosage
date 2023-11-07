e04a5e2c27671c116b16fbec6d74f82ecb2524b3
RomanYakovlev94 <80695880+RomanYakovlev94@users.noreply.github.com>
2023-11-07 12:34:36 +0300

    fixed save new user

--- backend/index.js
+++ backend/index.js
@@ -1,30 +1,28 @@
-import express from 'express';// Библиотека для бекенда (общение с фронтом и ДБ)
-import * as dotenv from 'dotenv'
+import express from "express"; // Библиотека для бекенда (общение с фронтом и ДБ)
+import * as dotenv from "dotenv";
 import mongoose, { connect } from "mongoose";
-import UserModel from './models/UserModel.js';
+import UserModel from "./models/UserModel.js";
 dotenv.config();
 
-
 const app = express();
-const DB_URL = process.env.CONNECT_MONGO
+const DB_URL = process.env.CONNECT_MONGO;
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));// в Express.js используется для подключения middleware, который отвечает за парсинг входящих запросов с данными, закодированными в формате application/x-www-form-urlencoded. Этот формат обычно используется HTML формами.
+app.use(express.urlencoded({ extended: true })); // в Express.js используется для подключения middleware, который отвечает за парсинг входящих запросов с данными, закодированными в формате application/x-www-form-urlencoded. Этот формат обычно используется HTML формами.
 //  app.use("/api", router);
 
-
 await mongoose.connect(DB_URL, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-	});
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
 
-app.get('/api/hello', (req, res) => {
-  // Отправляем строку 'hello' в ответе.
-  res.send('hello');
+app.get("/api/hello", (req, res) => {
+	// Отправляем строку 'hello' в ответе.
+	res.send("hello");
 });
 
 app.post("/api/users/register", async (req, res) => {
 	//отправляем пост запрос на сервер и передаем в бади логин и пароль
-	const createdUser = await userModel.create({
+	const createdUser = await UserModel.create({
 		login: req.body.login,
 		password: req.body.password,
 		name: req.body.name,
